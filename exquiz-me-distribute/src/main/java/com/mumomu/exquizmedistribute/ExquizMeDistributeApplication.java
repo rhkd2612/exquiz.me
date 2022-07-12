0a1f68e34815ac2e3e03b5fa3adfcc70ca717e7d
Lee Sang Bin <rhkd2612@naver.com>
2022-07-12 18:26:48 +0900

    project init

---
+++ exquiz-me-distribute/src/main/java/com/mumomu/exquizmedistribute/ExquizMeDistributeApplication.java
@@ -0,0 +1,13 @@
+package com.mumomu.exquizmedistribute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExquizMeDistributeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExquizMeDistributeApplication.class, args);
+	}
+
+}
---
+++ exquiz-me-distribute/src/test/java/com/mumomu/exquizmedistribute/ExquizMeDistributeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mumomu.exquizmedistribute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExquizMeDistributeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
