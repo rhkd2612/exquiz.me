4c74c1a597fd014df5095008f515284138f79ec3
netcopjr <netcopjr@gmail.com>
2022-07-18 01:16:30 +0900

    Removed unnecessary column names

--- exquizme/src/main/java/com/mumomu/exquizme/production/domain/Problemset.java
+++ exquizme/src/main/java/com/mumomu/exquizme/production/domain/Problemset.java
@@ -21,52 +21,28 @@ public class Problemset {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    //ProblemSet 작성자
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "host_id")
     private Host host;
 
-    //Problem 목록
     @OneToMany(mappedBy = "problemset", fetch = FetchType.LAZY)
     private List<Problem> problems;
 
-    //Room 목록
     @OneToMany(mappedBy = "problemset", fetch = FetchType.LAZY)
     private List<Room> rooms;
 
-    //태그 목록
     @OneToMany(mappedBy = "problemset")
     private List<ProblemsetTag> problemsetTags = new ArrayList<>();
 
-    //ProblemSet 제목
-    @Column(name = "title")
     private String title;
-
-    //ProblemSet 설명
-    @Column(name = "description")
     private String description;
+    private String closingMent;
 
-    //ProblemSet 종료 멘트
-    @Column(name = "closing")
-    private String closing;
-
-    //삭제 여부(비활성화 여부)
-    @Column(name = "deleted")
     private Boolean deleted;
 
-    //총 참가자 수
-    @Column(name = "total_participant")
-    private String totalParticipant;
-
-    //만든 날짜
-    @Column(name = "created_at")
     private Date createdAt;
-
-    //업데이트된 날짜
-    @Column(name = "updated_at")
     private Date updatedAt;
-
-    //삭제 일자
-    @Column(name = "deleted_at")
     private Date deletedAt;
+
+    private String totalParticipant;
 }
