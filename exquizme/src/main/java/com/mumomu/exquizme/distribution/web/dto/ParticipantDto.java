2a8c3e8f8a568055d5618368f9ba0a1e63e3ab20
Lee Sang Bin <rhkd2612@naver.com>
2022-07-21 06:17:04 +0900

    feat: participant dto changed
    feat: participant dto changed

--- exquizme/src/main/java/com/mumomu/exquizme/distribution/web/dto/ParticipantDto.java
+++ exquizme/src/main/java/com/mumomu/exquizme/distribution/web/dto/ParticipantDto.java
@@ -1,6 +1,7 @@
 package com.mumomu.exquizme.distribution.web.dto;
 
 import com.mumomu.exquizme.distribution.domain.Participant;
+import com.mumomu.exquizme.distribution.domain.Room;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,16 +11,20 @@ import java.util.Date;
 @Data
 @Builder @AllArgsConstructor
 public class ParticipantDto {
+    private Long id;
     private String uuid;
     private String name; // 사용자 구분 이름
     private String nickname;
+    private RoomDto roomDto;
     private Date entryDate; // 생성일(입장시간)
     private int currentScore; // 점수
 
     public ParticipantDto(Participant participant) {
+        this.id = participant.getId();
         this.uuid = participant.getUuid();
         this.name = participant.getName();
         this.nickname = participant.getNickname();
+        this.roomDto = new RoomDto(participant.getRoom());
         this.entryDate = participant.getEntryDate();
         this.currentScore = participant.getCurrentScore();
     }
