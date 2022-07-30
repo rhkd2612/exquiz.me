0ca016d8a3473a2298ac74c5423fc2347fec07e1
Lee Sang Bin <rhkd2612@naver.com>
2022-07-30 17:33:26 +0900

    fix: cors bug

--- src/main/java/com/mumomu/exquizme/config/WebConfig.java
+++ src/main/java/com/mumomu/exquizme/config/WebConfig.java
@@ -1,13 +1,15 @@
 package com.mumomu.exquizme.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("*")
+                .allowedOrigins("https://exquiz.me", "http://localhost:8080")
                 .allowedMethods("*")
                 .maxAge(3000);
     }
