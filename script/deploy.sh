2b67594f294336a4b03634c9f3d4e89135827e99
Lee Sang Bin <rhkd2612@naver.com>
2022-07-31 19:42:26 +0900

    cd: cd test - 10

--- script/deploy.sh
+++ script/deploy.sh
@@ -21,4 +21,4 @@ else
 fi
 
 echo "> $JAR_PATH deploy"
-nohup java -jar $JAR_PATH -Dspring.profiles.active=local > /dev/null 2> /dev/null < /dev/null &
+nohup java -jar $JAR_PATH --spring.profiles.active=dev > /dev/null 2> /dev/null < /dev/null &
