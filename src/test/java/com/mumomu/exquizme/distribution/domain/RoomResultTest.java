b914a505c0c178670c76469b1812d68cb69a7625
Lee Sang Bin <rhkd2612@naver.com>
2022-08-05 06:01:10 +0900

    fix: remove @Singular

--- src/test/java/com/mumomu/exquizme/distribution/domain/RoomResultTest.java
+++ src/test/java/com/mumomu/exquizme/distribution/domain/RoomResultTest.java
@@ -18,7 +18,7 @@ class RoomResultTest {
     @Test
     @Transactional
     void 외래키를기본키로() throws Exception{
-        Room room = Room.builder().pin("111111").build();
+        Room room = Room.ByBasicBuilder().pin("111111").build();
         RoomResult roomResult = RoomResult.builder().room(room).build();
 
         em.persist(roomResult);
