a219c34ed4986b6cd454debcc48476bbf55d3ab7
Lee Sang Bin <rhkd2612@naver.com>
2022-08-18 16:09:19 +0900

    feat: SockJS

--- src/main/java/com/mumomu/exquizme/config/WebConfig.java
+++ src/main/java/com/mumomu/exquizme/config/WebConfig.java
@@ -5,12 +5,12 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class  WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("*")
-                .allowedMethods("*")
+                //.allowedOrigins("*")
+                //.allowedMethods("*")
                 .maxAge(3000);
     }
 }
