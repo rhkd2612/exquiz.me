60c6a3b4f22a0b2d3293855c5d84da719b8543cd
Minkyum Kim <37054037+39dll@users.noreply.github.com>
2022-09-05 21:39:41 +0900

    feat. add image support

---
+++ src/main/java/com/mumomu/exquizme/production/crawling/exception/ResultNotFoundException.java
@@ -0,0 +1,5 @@
+package com.mumomu.exquizme.production.crawling.exception;
+
+public class ResultNotFoundException extends Exception {
+    public ResultNotFoundException(String message) { super(message); }
+}
